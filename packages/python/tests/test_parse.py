538ae5e981438556aa3aa936061816125f72895f
Christian Kongsgaard <kongsgaard.c@gmail.com>
2024-02-17 22:33:00 +0100

    fix: added generics to epdx.convert_ilcd

--- packages/python/tests/test_parse.py
+++ packages/python/tests/test_parse.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 import epdx
@@ -5,7 +7,14 @@ import epdx
 
 def test_parse_ilcd(datafix_dir):
     ilcd_file = datafix_dir / "f63ac879-fa7d-4f91-813e-e816cbdf1927.json"
-    epd = epdx.convert_ilcd(ilcd_file.read_text())
+    epd = epdx.convert_ilcd(ilcd_file.read_text(), as_type=dict)
+
+    assert isinstance(epd, dict)
+
+
+def test_parse_ilcd_dict_input(datafix_dir):
+    ilcd_file = datafix_dir / "f63ac879-fa7d-4f91-813e-e816cbdf1927.json"
+    epd = epdx.convert_ilcd(json.loads(ilcd_file.read_text()))
 
     assert isinstance(epd, dict)
 
@@ -17,13 +26,13 @@ def test_parse_empty():
 
 def test_parse_ilcd_str(datafix_dir):
     ilcd_file = datafix_dir / "f63ac879-fa7d-4f91-813e-e816cbdf1927.json"
-    epd = epdx.convert_ilcd(ilcd_file.read_text(), as_type='str')
+    epd = epdx.convert_ilcd(ilcd_file.read_text(), as_type=str)
 
     assert isinstance(epd, str)
 
 
 def test_parse_ilcd_pydantic(datafix_dir):
     ilcd_file = datafix_dir / "f63ac879-fa7d-4f91-813e-e816cbdf1927.json"
-    epd = epdx.convert_ilcd(ilcd_file.read_text(), as_type='pydantic')
+    epd = epdx.convert_ilcd(ilcd_file.read_text(), as_type=epdx.EPD)
 
     assert isinstance(epd, epdx.EPD)
